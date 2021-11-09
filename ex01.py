629045f7b9cb9d349bebd2741f2b425de99f0a2e
Welyk Diemerson <63251869+welykPereira@users.noreply.github.com>
2021-11-08 22:38:34 -0300

    Atualizei o exercicio 1

--- ex01.py
+++ ex01.py
@@ -1,7 +1,9 @@
-dias = int(input('Quantos dias?\t'))
-hrs = int(input('Qauntas hrs?\t'))
-minuto = int(input('Quantos minutos?\t'))
-seg = int(input('Quantos segundos?\t'))
-# um minuto contem 60 segundos, uma hora contem 60 min de 60 segundos e o dia contem 24 horas 60 minutos e 60 segundos
-total = seg + (minuto * 60) + (hrs * 60 * 60) + (dias * 24 *60 *60)
-print('O total de segundos calc52ulados e: {} '.format(total))
+
+
+x = 0.0
+y = 18
+print(type(x))
+print(type(y))
+z = x + y
+print('O resultado calculado: {} '.format(z))
+print('A variavel z veio do tipo: ', type(z))
