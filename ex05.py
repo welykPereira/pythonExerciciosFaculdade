03029c977ddb80a2d291534dd0d15b6f22fba5e1
Welyk Diemerson <63251869+welykPereira@users.noreply.github.com>
2021-11-08 23:25:49 -0300

    exercicio 05 resolvido

---
+++ ex05.py
@@ -0,0 +1,9 @@
+""""
+5. Escreva a sequência de comandos necessária para o cálculo da área de um triângulo de base 9 e altura 6.
+"""
+base = int(input("Digite a base do triangulo: \n"))
+altura = int(input("Digite a altura do triangulo \n"))
+
+area = base * altura / 2
+
+print("A area do triangulo e de: {} metros quadrados ".format(area))
