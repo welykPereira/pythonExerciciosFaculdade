174d4557dd4ce44dedf4f48dbcc7595195c9792c
Welyk Diemerson <63251869+welykPereira@users.noreply.github.com>
2021-11-28 00:41:35 -0300

    at

---
+++ while7.py
@@ -0,0 +1,35 @@
+
+
+while True:
+    nome = input('Digite seu nome?')
+    if nome != 'welyk':
+        continue
+    senha = input('Digite a senha')
+    if senha == 'shoow':
+        break
+print('Acesso liberado')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ whileAcumuladores.py
@@ -0,0 +1,9 @@
+cont = 1
+soma = 0
+
+while cont <= 5:
+    x = int(input('Digite o {} valor'.format(cont)))
+    soma += x
+    cont += 1
+print('O valor total e {} '.format(soma))
+
